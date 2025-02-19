f1abfb16f165f790ba65ccb2584fb42478ecee0d
ShreyasPC-GF63 <shreyasbp305@gmail.com>
2025-02-19 15:39:46 +0530

    19-02-2025: Added assimp lib

---
+++ OpenGL/src/Model.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <GL/glew.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <stb_image/stb_image.h>
+#include "../../Dependencies/assimp/include/assimp/Importer.hpp"
+#include <../../Dependencies/assimp/include/assimp/scene.h>
+#include <../../Dependencies/assimp/include/assimp/postprocess.h>
